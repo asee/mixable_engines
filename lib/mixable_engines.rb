54244c7d40610891c9654220ca088bcd7aa5ba45
Tim Morton <tim@timothymorton.com>
2011-09-01 17:16:43 -0400

    Removing application-specific code.

--- lib/mixable_engines.rb
+++ lib/mixable_engines.rb
@@ -4,12 +4,18 @@ module ActiveSupport::Dependencies
   def require_or_load(file_name, const_path = nil)
     if file_name.starts_with?(Rails.root.to_s + '/app')
       relative_name = file_name.gsub(Rails.root.to_s, '')
-      @engine_paths ||= NsfgrfpScoring::Application.railties.plugins.collect{|engine| engine.config.root.to_s }
+      @engine_paths ||= plugin_and_engine_paths
       @engine_paths.each do |path|
         engine_file = File.join(path, relative_name)
         require_or_load_without_multiple(engine_file, const_path) if File.file?(engine_file)
       end
     end
     require_or_load_without_multiple(file_name, const_path)
+  end
+  
+private
+  def plugin_and_engine_paths
+    engines = Rails::Application.railties.plugins + Rails::Application.railties.engines
+    engines.collect{|engine| engine.config.root.to_s }
   end
 end
